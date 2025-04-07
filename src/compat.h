884432ee2518b16bf09fc2e521d908db9befb91d
Mihai Moldovan <ionic@ionic.de>
2025-04-07 14:45:26 +0200

    src/compat.h: use new scheme, as shown on github/AD-Vega/qarv#22, which is arguably even better, because it lets us use Qt::... elements all the way!

--- src/compat.h
+++ src/compat.h
@@ -37,11 +37,12 @@ char *strndup (const char *s, size_t n);
 #endif /* defined (Q_OS_DARWIN) */
 
 #if QT_VERSION < QT_VERSION_CHECK (5, 14, 0)
-#define X2GO_COMPAT_SKIPEMPTYPARTS QString::SkipEmptyParts
-#define X2GO_COMPAT_KEEPEMPTYPARTS QString::KeepEmptyParts
-#else
-#define X2GO_COMPAT_SKIPEMPTYPARTS Qt::SkipEmptyParts
-#define X2GO_COMPAT_KEEPEMPTYPARTS Qt::KeepEmptyParts
+namespace Qt {
+  using SplitBehavior = QString::SplitBehavior;
+  const SplitBehavior SkipEmptyParts = SplitBehavior::SkipEmptyParts;
+  const SplitBehavior KeepEmptyParts = SplitBehavior::KeepEmptyParts;
+  const auto endl = ::endl;
+}
 #endif /* QT_VERSION < QT_VERSION_CHECK (5, 14, 0) */
 
 #endif /* !defined (COMPAT_H) */
