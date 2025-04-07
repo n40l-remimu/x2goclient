3b9823d7e9bdc4255a2fdfe536b1638a86538a64
Mihai Moldovan <ionic@ionic.de>
2025-04-07 11:00:07 +0200

    src/compat.h: it was fun while it lasted, but this scheme is too complicated and unnecessary.
    Switch to defining two macros and use these across the base later on.

--- src/compat.h
+++ src/compat.h
@@ -21,11 +21,6 @@
 #define COMPAT_H
 
 #include <QtCore/qglobal.h>
-#if QT_VERSION >= QT_VERSION_CHECK (5, 0, 0)
-#include <QtCore/qflags.h>
-#endif /* QT_VERSION >= QT_VERSION_CHECK (5, 0, 0) */
-#include <QString>
-#include <QStringList>
 
 #ifdef Q_OS_DARWIN
 /*
@@ -42,116 +37,11 @@ char *strndup (const char *s, size_t n);
 #endif /* defined (Q_OS_DARWIN) */
 
 #if QT_VERSION < QT_VERSION_CHECK (5, 14, 0)
-namespace Qt {
-  enum SplitBehaviorFlags {
-    SkipEmptyParts = QString::SkipEmptyParts,
-    KeepEmptyParts = QString::KeepEmptyParts,
-  };
-
-  Q_DECLARE_FLAGS (SplitBehavior, SplitBehaviorFlags)
-  Q_DECLARE_OPERATORS_FOR_FLAGS (SplitBehavior)
-}
-
-class QStringCompatWrapper : public QString {
-  public:
-    QStringCompatWrapper () {
-    }
-
-    QStringCompatWrapper (const QLatin1String &str) : QString (str) {
-    }
-
-    QStringCompatWrapper (const QString &str) : QString (str) {
-    }
-
-    QStringCompatWrapper (const char *str) : QString (str) {
-    }
-
-    Q_REQUIRED_RESULT
-    QStringList split(const QString &sep, Qt::SplitBehavior behavior = Qt::KeepEmptyParts,
-                      Qt::CaseSensitivity cs = Qt::CaseSensitive) const {
-      return (QString::split (sep, QStringCompatWrapper::mapSplitBehavior (behavior), cs));
-    }
-    Q_REQUIRED_RESULT
-    QStringList split(QChar sep, Qt::SplitBehavior behavior = Qt::KeepEmptyParts,
-                      Qt::CaseSensitivity cs = Qt::CaseSensitive) const {
-      return (QString::split (sep, QStringCompatWrapper::mapSplitBehavior (behavior), cs));
-    }
-#if QT_VERSION >= QT_VERSION_CHECK (5, 0, 0)
-    Q_REQUIRED_RESULT
-    QVector<QStringRef> splitRef(const QString &sep,
-                                 Qt::SplitBehavior behavior = Qt::KeepEmptyParts,
-                                 Qt::CaseSensitivity cs = Qt::CaseSensitive) const {
-      return (QString::splitRef (sep, QStringCompatWrapper::mapSplitBehavior (behavior), cs));
-    }
-    Q_REQUIRED_RESULT
-    QVector<QStringRef> splitRef(QChar sep, Qt::SplitBehavior behavior = Qt::KeepEmptyParts,
-                                 Qt::CaseSensitivity cs = Qt::CaseSensitive) const {
-      return (QString::splitRef (sep, QStringCompatWrapper::mapSplitBehavior (behavior), cs));
-    }
-#endif /* QT_VERSION >= QT_VERSION_CHECK (5, 0, 0) */
-#ifndef QT_NO_REGEXP
-    Q_REQUIRED_RESULT
-    QStringList split(const QRegExp &sep,
-                      Qt::SplitBehavior behavior = Qt::KeepEmptyParts) const {
-      return (QString::split (sep, QStringCompatWrapper::mapSplitBehavior (behavior)));
-    }
-#if QT_VERSION >= QT_VERSION_CHECK (5, 0, 0)
-    Q_REQUIRED_RESULT
-    QVector<QStringRef> splitRef(const QRegExp &sep,
-                                 Qt::SplitBehavior behavior = Qt::KeepEmptyParts) const {
-      return (QString::splitRef (sep, QStringCompatWrapper::mapSplitBehavior (behavior)));
-    }
-#endif /* QT_VERSION >= QT_VERSION_CHECK (5, 0, 0) */
-#endif /* !defined (QT_NO_REGEXP) */
-#if QT_VERSION >= QT_VERSION_CHECK (5, 0, 0)
-#ifndef QT_NO_REGULAREXPRESSION
-    Q_REQUIRED_RESULT
-    QStringList split(const QRegularExpression &sep,
-                      Qt::SplitBehavior behavior = Qt::KeepEmptyParts) const {
-      return (QString::split (sep, QStringCompatWrapper::mapSplitBehavior (behavior)));
-    }
-    Q_REQUIRED_RESULT
-    QVector<QStringRef> splitRef(const QRegularExpression &sep,
-                                 Qt::SplitBehavior behavior = Qt::KeepEmptyParts) const {
-      return (QString::splitRef (sep, QStringCompatWrapper::mapSplitBehavior (behavior)));
-    }
-#endif /* !defined (QT_NO_REGULAREXPRESSION) */
-#endif /* QT_VERSION >= QT_VERSION_CHECK (5, 0, 0) */
-
-  private:
-    static QString::SplitBehavior mapSplitBehavior (Qt::SplitBehavior behavior) {
-      QString::SplitBehavior ret = QString::KeepEmptyParts;
-
-      if (behavior & Qt::SkipEmptyParts) {
-        ret = QString::SkipEmptyParts;
-      }
-
-      return (ret);
-    }
-};
-
-class QStringListCompatWrapper : public QStringList {
-  public:
-    QStringListCompatWrapper () {
-    }
-
-    QStringListCompatWrapper (const QStringList &list) : QStringList (list) {
-    }
-
-    QStringListCompatWrapper (const QStringCompatWrapper &str) : QStringList (str) {
-    }
-
-    const QStringCompatWrapper& operator[] (int i) const {
-      return (static_cast<const QStringCompatWrapper&> (this->operator[] (i)));
-    }
-
-    QStringCompatWrapper& operator[] (int i) {
-      return (static_cast<QStringCompatWrapper&> (this->operator[] (i)));
-    }
-};
-
-#define QString QStringCompatWrapper
-#define QStringList QStringListCompatWrapper
+#define X2GO_COMPAT_SKIPEMPTYPARTS QString::SkipEmptyParts
+#define X2GO_COMPAT_KEEPEMPTYPARTS QString::KeepEmptyParts
+#else
+#define X2GO_COMPAT_SKIPEMPTYPARTS Qt::SkipEmptyParts
+#define X2GO_COMPAT_KEEPEMPTYPARTS Qt::KeepEmptyParts
 #endif /* QT_VERSION < QT_VERSION_CHECK (5, 14, 0) */
 
 #endif /* !defined (COMPAT_H) */
